11734b199c858431f57c201bca6e21112ce1069f
Justin Henry <justin.henry@uvm.edu>
2020-10-20 15:17:54 -0400

    Additonal logging.

--- bin/q_pipe.pl
+++ bin/q_pipe.pl
@@ -6,14 +6,19 @@ my $job=$ARGV[2];
 $job =~ s/--video=// ;
 my $q_num=$job % 7;
 
-my $Config = Config::Tiny->new;
-$Config = Config::Tiny->read( 'config.config' );
-my daemonpath = $Config->{_}->{daemonpath};
+use File::Basename;
+my $dirname = dirname(__FILE__);
+my $configFile = "$dirname/config.config";
+
+my $config = Config::Tiny->read( $configFile );
+my $daemonpath = $config->{_}->{daemonpath};
 
 # Set path to queue
 my $q_file="$daemonpath/$q_num";
 
+print "Piping new encode into the queue at $q_file...\n";
 my $q = new File::Queue (File => $q_file,Mode=>0774 );
+
 my $command=join(' ',@ARGV);
 print "Job $job Q $q_num\n";
 print "command $command \n";
